1d9f47c04bed1c5791846eab1b3bc7275a76f6e7
sprungsumobr <48767935+sprungsumobr@users.noreply.github.com>
2022-04-13 21:19:52 -0300

    Initial commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\PessoasModel;
+
+class Home extends BaseController
+{
+    
+    public function index()
+    {
+        echo view('template/header');
+        echo view('home');
+        echo view('template/footer');
+    }
+
+    public function page($page='home'){
+        
+        echo view('template/header');
+        echo view($page);
+        echo view('template/footer');
+    }
+
+    public function pessoas(){
+        $model = new PessoasModel();
+
+        $data = [
+            'title'=>'Pessoas',
+            'pessoas'=>$model->getPessoas()
+        ];
+
+        echo view('template/header');
+        echo view('pessoa',$data);
+        echo view('template/footer');
+    }
+}
